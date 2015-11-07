46b03d5d03e143c2e70abbe549d7b18ae85a474c
Joe Osburn <joe@jnodev.com>
2015-11-07 00:21:47 -0600

    Refactor middleware; update so all middleware runs unless they return false

--- lib/cannon/handler.rb
+++ lib/cannon/handler.rb
@@ -36,45 +36,4 @@ module Cannon
       build_middleware_runner(middleware, callback: middleware_runner)
     end
   end
-
-  class MiddlewareRunner
-    include EventMachine::Deferrable
-
-    def initialize(ware, callback:, app:)
-      @app = app
-      @ware, @callback = instantiate(ware), callback
-    end
-
-    def run(request, response)
-      @ware.run(request, response)
-
-      if response.sent?
-        self.fail
-      else
-        setup_callback
-        self.succeed(request, response)
-      end
-    end
-
-  private
-
-    def setup_callback
-      set_deferred_status nil
-      callback do |request, response|
-        @callback.run(request, response) unless @callback.nil?
-      end
-    end
-
-    def instantiate(ware)
-      if ware.is_a?(String)
-        begin
-          Object.const_get(ware).new(@app)
-        rescue NameError
-          Object.const_get("Cannon::Middleware::#{ware}").new(@app)
-        end
-      else
-        ware.new(@app)
-      end
-    end
-  end
 end
