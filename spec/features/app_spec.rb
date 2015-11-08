d5fb329dc3014f19dc3c2ff847607c0d73a39fc1
Joe Osburn <joe@jnodev.com>
2015-11-07 23:58:28 -0600

    Improved specs to cover views, files, 404s, and 500s

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -12,11 +12,24 @@ def are_you(request, response)
   response.send('are you?')
 end
 
+def test_view(request, response)
+  response.view('test.html')
+end
+
+def raise_500(request, response)
+  bad_fail_code
+end
+
 RSpec.describe 'Cannon app' do
   before(:all) do
     cannon_app do |app|
       app.get('/hi', action: 'hi')
       app.get('/how', actions: ['hi', 'how', 'are_you'])
+      app.get('/view', action: 'test_view')
+      app.get('/bad', action: 'raise_500')
+
+      app.view_path = '../fixtures/views'
+      app.public_path = '../fixtures/public'
     end
   end
 
@@ -41,6 +54,30 @@ RSpec.describe 'Cannon app' do
       get '/how'
       expect(response.code).to eq('200')
       expect(response.body).to eq('hi how are you?')
+    end
+
+    it 'renders a view' do
+      get '/view'
+      expect(response.body).to eq('Test view content')
+      expect(response['Content-Type']).to eq('text/html')
+    end
+
+    it 'serves files' do
+      get '/background.jpg'
+      expect(response.body.size).to_not eq('')
+      expect(response.code).to eq('200')
+      expect(response['Content-Type']).to eq('image/jpeg')
+      expect(response['Content-Length']).to eq('55697')
+    end
+
+    it 'returns 404 for not found routes' do
+      get '/badroute'
+      expect(response.code).to eq('404')
+    end
+
+    it 'returns 500 for errors' do
+      get '/bad'
+      expect(response.code).to eq('500')
     end
   end
 end
