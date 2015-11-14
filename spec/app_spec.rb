20cb84e10955ad4e63dfdab8af0e66c0a81ad7a6
Joe Osburn <joe@jnodev.com>
2015-11-14 12:13:19 -0600

    Cleanup app_spec listening tests to work with new async code

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,24 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe Cannon::App do
-  let!(:app) do
-    app = Cannon::App.new(binding)
-    app.config.log_level = :error
-    app
-  end
-
   context 'app is listening asynchronously' do
-    before(:each) { app.listen(async: true, port: 3030) }
+    before(:each) { cannon_app.listen(port: 3030, async: true) }
+    after(:each) { cannon_app.stop }
 
     it 'raises an error if trying to listen again before stopping' do
-      expect(-> { app.listen }).to raise_error(Cannon::AlreadyListening)
+      expect(-> { cannon_app.listen }).to raise_error(Cannon::AlreadyListening)
     end
 
     context 'app has been stopped' do
-      before(:each) { app.stop }
+      before(:each) { cannon_app.stop }
 
       it 'does not raise an error' do
-        expect(-> { app.listen }).to_not raise_error
+        expect(-> { cannon_app.listen(async: true) }).to_not raise_error
       end
     end
   end
