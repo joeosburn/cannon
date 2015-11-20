b41dd6884aebac0f4c796d5166fea081f7662cb4
Joe Osburn <joe@jnodev.com>
2015-11-20 00:44:09 -0600

    Add basic mustache rendering support

--- lib/cannon/views.rb
+++ lib/cannon/views.rb
@@ -1,11 +1,13 @@
+require 'mustache'
+
 module Cannon
   module Views
-    def view(filename, status: :ok)
+    def view(filename, options = {})
       filepath = "#{@view_path}/#{filename}"
       view_data = IO.binread(filepath)
       mime_type = Cannon.mime_type(filepath)
       header('Content-Type', mime_type) if mime_type
-      send(view_data, status: status)
+      send(Mustache.render(view_data, options), status: status)
     end
   end
 end
