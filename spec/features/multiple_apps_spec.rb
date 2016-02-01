2e99ce1ec6ff7e73223e42684d54deb210f32ea6
Joe Osburn <joe@jnodev.com>
2016-01-31 23:23:25 -0600

    Add spec to confirm each app can have their own configuration

--- spec/features/multiple_apps_spec.rb
+++ spec/features/multiple_apps_spec.rb
@@ -12,8 +12,17 @@ RSpec.describe 'Multiple apps at once', :cannon_app do
       response.send('app2 info')
     end
 
+    app3 = Cannon::App.new(binding, port: 5033, ip_address: '127.0.0.1')
+    app3.runtime.config.log_level = :fatal
+    app3.config.middleware = %w{RequestLogger Files Session Flash Router ContentType}
+    app3.get('/info') do |request, response|
+      request.cookies
+      response.send('app2 info')
+    end
+
     cannon_app.listen(async: true)
     app2.listen(async: true)
+    app3.listen(async: true)
   end
 
   it 'allows requests to multiple cannon apps at the same time' do
@@ -22,5 +31,10 @@ RSpec.describe 'Multiple apps at once', :cannon_app do
 
     get '/info', port: 5032
     expect(response.body).to eq('app2 info')
+  end
+
+  it 'allows each app to have their own config' do
+    get '/info', port: 5033
+    expect(response.code).to be(500) # confirm cookies middleware is not present
   end
 end
