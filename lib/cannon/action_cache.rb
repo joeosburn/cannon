e0f498327637fb465fa28d0848ffabe814a9d847
Joe Osburn <joe@jnodev.com>
2016-02-28 22:06:48 -0600

    Add support for busting action cache

--- lib/cannon/action_cache.rb
+++ lib/cannon/action_cache.rb
@@ -5,13 +5,13 @@ module Cannon
     end
 
     def handle_route_action(route_action, request:, response:, next_proc:)
-      if request.method == 'GET' && !route_action.action.is_a?(Proc) && cached?(route_action)
+      if request.method == 'GET' && !route_action.action.is_a?(Proc) && cached?(route_action.action)
         run_action_cache(request, response, route_action: route_action)
         next_proc.call
       else
         cached_next_proc = lambda do
           response.delegated_response.stop_recording
-          @cache[cache_key(route_action)] = response.delegated_response.recording
+          @cache[cache_key(route_action.action)] = response.delegated_response.recording
           next_proc.call
         end
         response.delegated_response.start_recording
@@ -19,20 +19,24 @@ module Cannon
       end
     end
 
-    def cached?(route_action)
-      @cache.include? cache_key(route_action)
+    def delete(action)
+      @cache.delete(cache_key(action)) if cached?(action)
+    end
+
+    def cached?(action)
+      @cache.include? cache_key(action)
     end
 
   private
 
     def run_action_cache(request, response, route_action:)
-      @cache[cache_key(route_action)].each do |sym, args, block|
+      @cache[cache_key(route_action.action)].each do |sym, args, block|
         response.delegated_response.send(sym, *args, &block)
       end
     end
 
-    def cache_key(route_action)
-      "action_cache_#{route_action.action}"
+    def cache_key(action)
+      "action_cache_#{action}"
     end
   end
 end
