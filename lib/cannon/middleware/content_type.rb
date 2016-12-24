4afbb181a3b12e3cfc216d4b460db061310c849e
Joe Osburn <joe@jnodev.com>
2016-12-24 09:56:12 -0600

    Refactor and cleanup content_type.rb

--- lib/cannon/middleware/content_type.rb
+++ lib/cannon/middleware/content_type.rb
@@ -1,12 +1,18 @@
 module Cannon
   module Middleware
+    # Middleware which sets a default content type of text/plain if no content type has been set
     class ContentType
       def initialize(app)
         @app = app
       end
 
       def run(request, response, next_proc)
-        return next_proc.call unless response.headers['Content-Type'].nil?
+        response.headers['Content-Type'] ? next_proc.call : handle(request, response, next_proc)
+      end
+
+    private
+
+      def handle(_request, response, next_proc)
         response.headers['Content-Type'] = 'text/plain; charset=us-ascii'
         next_proc.call
       end
